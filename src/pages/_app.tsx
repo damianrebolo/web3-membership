705c77ecf7821dc61d8897fe24185d6a091135b9
Damian Rebolo <damianrebolo@gmail.com>
2022-12-03 15:00:33 +0800

    feat: configure thirdweb provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Goerli}
+      chainRpc={{
+        [ChainId.Goerli]: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`,
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
