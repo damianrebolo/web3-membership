30cb65c81b6cf0972313778f57d7b22711a466ef
Damian Rebolo <damianrebolo@gmail.com>
2022-12-04 08:38:34 +0800

    feat: useNFTMinted Hook to get all NFTs Minted

--- src/hooks/useNFTMinted.ts
+++ src/hooks/useNFTMinted.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 
-import { EditionDrop, NFT } from "@thirdweb-dev/sdk";
+import { NFT } from "@thirdweb-dev/sdk";
+import { useContract } from "@thirdweb-dev/react";
 
-export const useNFTMinted = (contract: EditionDrop | undefined) => {
+export const useNFTMinted = () => {
   const [nfts, setNFTs] = useState<NFT[]>([]);
+  const { contract, isLoading } = useContract("0xe21Bd82Ce2f1D4116B263910A12c906d42A10cfA", "edition-drop");
 
   useEffect(() => {
     if (!contract) return;
@@ -15,5 +17,5 @@ export const useNFTMinted = (contract: EditionDrop | undefined) => {
     getAllNFTs();
   }, [contract]);
 
-  return { nfts };
+  return { nfts, isLoading };
 };
