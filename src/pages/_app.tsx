26042046d0319458b451d38980595d977ff441b0
Damian Rebolo <damianrebolo@gmail.com>
2022-12-04 09:56:36 +0800

    feat: Add Layout on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Layout } from "@components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         [ChainId.Goerli]: `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`,
       }}
     >
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThirdwebProvider>
   );
 }
